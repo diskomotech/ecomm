a1a65b41568b9b3fa7c775929b1ef101dbd23c22
Jim Wright <jimwright808@gmail.com>
2020-08-01 10:02:54 +0100

    create comparePassword function

--- repositories/users.js
+++ repositories/users.js
@@ -42,6 +42,16 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    // Saved -> password in our database. 'hashed.salt'
+    // Supplied -> password given to us by user trying to sign in
+
+    const [hashed, salt] = saved.split('.');
+    const hashedSupplied = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSupplied.toString('hex');
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
   }
