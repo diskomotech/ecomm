ac5ce2c54f644a46899b7bb892d858ae453c1de0
Jim Wright <jimwright808@gmail.com>
2020-08-26 09:21:55 +0100

    create user products logic

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -72,4 +72,10 @@ async (req, res) => {
   res.redirect('/admin/products');
 });
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect('/admin/products');
+});
+
 module.exports = router;
