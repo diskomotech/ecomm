7a5fafb3d123392b6d37e147638bb5fb3bf262e7
Jim Wright <jimwright808@gmail.com>
2020-08-30 10:37:59 +0100

    create shopping cart logic

---
+++ routes/carts.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cartsRepo = require('../repositories/carts');
+
+const router = express.Router();
+
+// Receive a POST request to add an item to a cart
+router.post('/cart/products', async (req, res) => {
+  // Figure out the cart
+  let cart;
+  if (!req.session.cartId) {
+    // We don't have a cart, we need to create one
+    // and store the cart id on req.session.cartId
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // We have a cart. Lets get it from the repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  console.log(cart);
+  // Either increment quantity for existing product 
+  // or add new product to items array
+
+
+  res.send('Product added to cart');
+});
+
+// Receive a GET request to show all items in cart
+
+
+// Receive a POST request to delete an item from a cart
+
+
+module.exports = router;
